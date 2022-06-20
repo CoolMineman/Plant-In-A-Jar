afdd97749ca2b15b497e84e2a1b6f44dd0426a8d
CoolMineman <62723322+CoolMineman@users.noreply.github.com>
2022-06-19 19:18:17 -0500

    fix merge

--- src/main/java/io/github/coolmineman/plantinajar/fake/FakeChunkGenerator.java
+++ src/main/java/io/github/coolmineman/plantinajar/fake/FakeChunkGenerator.java
@@ -11,13 +11,13 @@ import net.minecraft.world.ChunkRegion;
 import net.minecraft.world.HeightLimitView;
 import net.minecraft.world.Heightmap.Type;
 import net.minecraft.world.biome.source.BiomeAccess;
-import net.minecraft.world.biome.source.util.MultiNoiseUtil.MultiNoiseSampler;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.gen.GenerationStep.Carver;
 import net.minecraft.world.gen.StructureAccessor;
 import net.minecraft.world.gen.chunk.Blender;
 import net.minecraft.world.gen.chunk.ChunkGenerator;
 import net.minecraft.world.gen.chunk.VerticalBlockSample;
+import net.minecraft.world.gen.noise.NoiseConfig;
 
 public class FakeChunkGenerator extends ChunkGenerator {
     final FakeServerWorld world;
@@ -33,26 +33,6 @@ public class FakeChunkGenerator extends ChunkGenerator {
     }
 
     @Override
-    public ChunkGenerator withSeed(long var1) {
-        return this;
-    }
-
-    @Override
-    public MultiNoiseSampler getMultiNoiseSampler() {
-        return null;
-    }
-
-    @Override
-    public void carve(ChunkRegion var1, long var2, BiomeAccess var4, StructureAccessor var5, Chunk var6, Carver var7) {
-        // noop
-    }
-
-    @Override
-    public void buildSurface(ChunkRegion var1, StructureAccessor var2, Chunk var3) {
-        // noop
-    }
-
-    @Override
     public void populateEntities(ChunkRegion var1) {
         // noop
     }
@@ -60,11 +40,6 @@ public class FakeChunkGenerator extends ChunkGenerator {
     @Override
     public int getWorldHeight() {
         return world.getHeight();
-    }
-
-    @Override
-    public CompletableFuture<Chunk> populateNoise(Executor var1, Blender var2, StructureAccessor var3, Chunk var4) {
-        return null;
     }
 
     @Override
@@ -78,17 +53,32 @@ public class FakeChunkGenerator extends ChunkGenerator {
     }
 
     @Override
-    public int getHeight(int var1, int var2, Type var3, HeightLimitView var4) {
-        return 0;
+    public void carve(ChunkRegion var1, long var2, NoiseConfig var4, BiomeAccess var5, StructureAccessor var6, Chunk var7, Carver var8) {
+        // noop
     }
 
     @Override
-    public VerticalBlockSample getColumnSample(int var1, int var2, HeightLimitView var3) {
+    public void buildSurface(ChunkRegion var1, StructureAccessor var2, NoiseConfig var3, Chunk var4) {
+        // noop
+    }
+
+    @Override
+    public CompletableFuture<Chunk> populateNoise(Executor var1, Blender var2, NoiseConfig var3, StructureAccessor var4, Chunk var5) {
         return null;
     }
 
     @Override
-    public void getDebugHudText(List<String> var1, BlockPos var2) {
+    public int getHeight(int x, int z, Type var3, HeightLimitView var4, NoiseConfig var5) {
+        return world.getTopY(var3, x, z);
+    }
+
+    @Override
+    public VerticalBlockSample getColumnSample(int var1, int var2, HeightLimitView var3, NoiseConfig var4) {
+        return null;
+    }
+
+    @Override
+    public void getDebugHudText(List<String> var1, NoiseConfig var2, BlockPos var3) {
         // noop
     }
     
